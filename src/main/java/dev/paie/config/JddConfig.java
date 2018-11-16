c8062d4c3e88ae2d16aa40246dc4ff5ca6bbe829
Bastianelli_Matthieu <m.bastianelli@laposte.net>
2018-11-16 09:44:50 +0100

    Il manque l'assert du Net à payer, erreur d'environ 13€

---
+++ src/main/java/dev/paie/config/JddConfig.java
@@ -0,0 +1,10 @@
+package dev.paie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("jdd-config.xml")
+public class JddConfig {
+
+}
