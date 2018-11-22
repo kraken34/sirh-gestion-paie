1337336558b0672337c3ed8b75d0b381ead5701c
Champetier <todave11@gmail.com>
2018-11-22 17:46:17 +0100

    next

--- src/main/java/dev/paie/repository/CotisationRepository.java
+++ src/main/java/dev/paie/repository/CotisationRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.paie.entite.Cotisation;
-
+//bug
 public interface CotisationRepository extends JpaRepository<Cotisation, Integer> {
 	List<Cotisation> findByCode(String code);
 
