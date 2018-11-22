ad72a27e455ac6e825915948c84a7aeeca7fe52c
Champetier <todave11@gmail.com>
2018-11-22 18:09:29 +0100

    config HerokuDBConfig

--- src/main/java/dev/paie/config/JpaConfig.java
+++ src/main/java/dev/paie/config/JpaConfig.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@Import(DataSourceMySQLConfig.class)
+@Import({DataSourceMySQLConfig.class,HerokuDBConfig.class})
 @EnableTransactionManagement
 public class JpaConfig {
 	
