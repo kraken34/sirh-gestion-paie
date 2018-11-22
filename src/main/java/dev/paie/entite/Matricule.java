53189990a9f626a76d4c4704c6ff8ce1fc42539c
Bastianelli_Matthieu <m.bastianelli@laposte.net>
2018-11-22 16:33:38 +0100

    String à la place du ZoneDateTimeDansLaMatricule.

--- src/main/java/dev/paie/entite/Matricule.java
+++ src/main/java/dev/paie/entite/Matricule.java
@@ -1,6 +1,5 @@
 package dev.paie.entite;
 
-import java.time.ZonedDateTime;
 import java.util.List;
 
 public class Matricule {
@@ -10,12 +9,85 @@ public class Matricule {
 	String nom;
 	String prenom;
 	String email;
-	ZonedDateTime dateNaissance;
+	String dateNaissance;
 	String sexe;
 	String adresse;
 	String password;
 	String image;
 	List<String> subalternes;
-	String departement;	
+	String departement;
+	
+	
+	public String getMatricule() {
+		return matricule;
+	}
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public String getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(String dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public String getSexe() {
+		return sexe;
+	}
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public List<String> getSubalternes() {
+		return subalternes;
+	}
+	public void setSubalternes(List<String> subalternes) {
+		this.subalternes = subalternes;
+	}
+	public String getDepartement() {
+		return departement;
+	}
+	public void setDepartement(String departement) {
+		this.departement = departement;
+	}	
+	
+	
+	
 
 }
