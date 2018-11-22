1b0af8d220b1b0ff50f73367c0eeb51c2a7a5c6b
nicolas.schauss <nicolas.schauss@gmail.com>
2018-11-22 17:42:52 +0100

    Rest API use & application publication

---
+++ src/main/java/dev/paie/entite/Collegue.java
@@ -0,0 +1,149 @@
+package dev.paie.entite;
+
+public class Collegue {
+
+	private String matricule;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String dateNaissance;
+	private String sexe;
+	private String adresse;
+	private String password;
+	private String photo;
+	private String[] subalternes;
+	private String departement;
+	
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return adresse;
+	}
+	/**
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	/**
+	 * @return the photo
+	 */
+	public String getPhoto() {
+		return photo;
+	}
+	/**
+	 * @param photo the photo to set
+	 */
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	/**
+	 * @return the matricule
+	 */
+	public String getMatricule() {
+		return matricule;
+	}
+	/**
+	 * @param matricule the matricule to set
+	 */
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	/**
+	 * @return the dateNaissance
+	 */
+	public String getDateNaissance() {
+		return dateNaissance;
+	}
+	/**
+	 * @param dateNaissance the dateNaissance to set
+	 */
+	public void setDateNaissance(String dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	/**
+	 * @return the sexe
+	 */
+	public String getSexe() {
+		return sexe;
+	}
+	/**
+	 * @param sexe the sexe to set
+	 */
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the subalternes
+	 */
+	public String[] getSubalternes() {
+		return subalternes;
+	}
+	/**
+	 * @param subalternes the subalternes to set
+	 */
+	public void setSubalternes(String[] subalternes) {
+		this.subalternes = subalternes;
+	}
+	/**
+	 * @return the departement
+	 */
+	public String getDepartement() {
+		return departement;
+	}
+	/**
+	 * @param departement the departement to set
+	 */
+	public void setDepartement(String departement) {
+		this.departement = departement;
+	}
+}
--- src/main/java/dev/paie/web/controller/RemunerationEmployeController.java
+++ src/main/java/dev/paie/web/controller/RemunerationEmployeController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import dev.paie.entite.Collegue;
 import dev.paie.entite.RemunerationEmploye;
 import dev.paie.repository.EntrepriseRepository;
 import dev.paie.repository.GradeRepository;
@@ -36,8 +38,18 @@ public class RemunerationEmployeController {
 	
 	@RequestMapping(method=RequestMethod.POST, path="/creer")
 	public ModelAndView ajouterEmploye(@ModelAttribute("employe") RemunerationEmploye employe) {
-		remunerationEmployeRepository.save(employe);
-		return creerEmploye();
+		
+		RestTemplate rt = new RestTemplate();
+		Collegue[] result = rt.getForObject("http://collegues-api.cleverapps.io/collegues?matricule="+employe.getMatricule(), Collegue[].class);
+		
+		if ( result.length > 0)
+		{
+			remunerationEmployeRepository.save(employe);
+			return creerEmploye();
+		}
+		else
+			return mauvaisMatricule();
+		
 	}
 	
 	@RequestMapping(method=RequestMethod.GET, path="/lister")
@@ -45,6 +57,17 @@ public class RemunerationEmployeController {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("employes/listerEmployes");
 		mv.addObject("listeEmploye",remunerationEmployeRepository.findAll());
+		return mv;
+	}
+	
+	public ModelAndView mauvaisMatricule() {
+		RestTemplate rt = new RestTemplate();
+		Collegue[] result = rt.getForObject("http://collegues-api.cleverapps.io/collegues", Collegue[].class);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("employes/listerMatricules");
+		mv.addObject("listeCollegues",result);
+		
 		return mv;
 	}
 }
